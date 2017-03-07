b0774f0d0f9f86a6d80fb974f66c71063fe508ca
Wolfgang Kling <wkling@rbsas.co>
2017-03-07 10:29:14 -0500

    Se agrega datepicker

--- src/client/app/module-one.module/usecase2/main.component.ts
+++ src/client/app/module-one.module/usecase2/main.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//https://github.com/kekeh/mydatepicker#locale-attribute
+import { IMyOptions, IMyDate, IMyDateModel, IMyCalendarViewChanged } from 'mydatepicker';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     moduleId: module.id,
@@ -6,6 +9,63 @@ import { Component } from '@angular/core';
     templateUrl: './main.component.html',
 })
 
-export class MainComponent {
+export class MainComponent implements OnInit {
+    valid: false;
+    private myForm: FormGroup;
+    private locale: string = 'es';
+    private selDate: IMyDate = { year: 2002, month: 9, day: 11 };
 
+    private myDatePickerOptions: IMyOptions = {
+        dateFormat: 'dd/mm/yyyy',
+        showTodayBtn: true,
+    };
+
+    private selectedTextInline: string = '';
+    private border: string = 'none';
+
+
+    constructor(private formBuilder: FormBuilder) {
+    }
+
+    ngOnInit() {
+        this.myForm = this.formBuilder.group({
+            myDate: [this.selDate, Validators.required]
+        });
+    }
+
+    onDateChanged(event: IMyDateModel) {
+        console.log('onDateChanged(): ', event.date, ' - jsdate: ', new Date(event.jsdate).toLocaleDateString(), ' - formatted: ', event.formatted, ' - epoc timestamp: ', event.epoc);
+        if (event.formatted !== '') {
+            this.selectedTextInline = 'Formatted: ' + event.formatted + ' - epoc timestamp: ' + event.epoc;
+            this.border = '1px solid #CCC';
+        }
+        else {
+            this.selectedTextInline = '';
+            this.border = 'none';
+        }
+    }
+
+    onCalendarViewChanged(event: IMyCalendarViewChanged) {
+        console.log('onCalendarViewChanged(): Year: ', event.year, ' - month: ', event.month, ' - first: ', event.first, ' - last: ', event.last);
+    }
+
+    onCalendarToggle(event: number): void {
+        console.log('onCalendarToggle(): Reason: ', event);
+    }
+
+    getClassForClassGroup() {
+        if (this.valid) {
+            return 'has-success';
+        } else {
+            return 'has-danger';
+        }
+    }
+
+    getClassForInput() {
+        if (this.valid) {
+            return 'form-control-success';
+        } else {
+            return 'form-control-danger';
+        }
+    }
 }
--- src/client/app/module-one.module/usecase2/usecase2.module.ts
+++ src/client/app/module-one.module/usecase2/usecase2.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MainComponent } from './main.component';
+
+//ng2-bootstrap
+import { AlertModule } from 'ng2-bootstrap';
+
+import { MyDatePickerModule } from 'mydatepicker';
 
 @NgModule({
     imports: [
         RouterModule,
+        AlertModule,
+        CommonModule,
+        MyDatePickerModule,
+        FormsModule,
+        ReactiveFormsModule,
     ],
-    declarations: [MainComponent],
-    exports: [MainComponent],
+    declarations: [
+        MainComponent,
+    ],
+    exports: [
+        MainComponent,
+    ],
 })
 
 export class Usecase2Module {
