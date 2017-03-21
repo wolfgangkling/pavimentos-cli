8a4aa211d67e9fea4c696993393101140dd97429
Wolfgang Kling <wkling@rbsas.co>
2017-03-21 09:07:40 -0500

    Se solucionó problema al cambiar valor en el combo de confiabiidad

--- src/client/app/module-one.module/usecase1/main.component.ts
+++ src/client/app/module-one.module/usecase1/main.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { CustomValidators } from 'ng2-validation';
 import { Pavimento } from './pavimento.model';
 
@@ -19,6 +19,7 @@ export class MainComponent implements OnInit {
 
     myForm: FormGroup;
     confiabDisenoOptions: string[];
+    numestruc : Number = 0;
 
     constructor(
         private _fb: FormBuilder,
@@ -31,17 +32,60 @@ export class MainComponent implements OnInit {
             ejesequiv: ['', [Validators.required, CustomValidators.range([0, 10000000])]],
             confiabdiseno: ['', [Validators.required]],
             errestandar: ['', [Validators.required, CustomValidators.range([0, 1])]],
-            modresili: ['', [Validators.required, CustomValidators.min(0)]],
+            modresili: ['', [Validators.required, CustomValidators.gt(0)]],
             servicini: ['', [Validators.required, CustomValidators.range([0, 5])]],
             servicfin: ['', [Validators.required, CustomValidators.range([0, 5])]],
             numestruc: [],
         });
         this.confiabDisenoOptions = this.getConfiabDisenoOptions();
+
+        //Delete .. jus for testing purposes
+        let pavimento: Pavimento = {
+            ejesequiv: 7950000,
+            confiabdiseno: '95.00',
+            errestandar: 0.49,
+            modresili: 90,
+            servicini: 4.5,
+            servicfin: 2.8,
+            numestruc: 0,
+        }
+
+        this.myForm.controls['ejesequiv'].setValue(pavimento.ejesequiv);
+        this.myForm.controls['confiabdiseno'].setValue(pavimento.confiabdiseno);
+        this.myForm.controls['errestandar'].setValue(pavimento.errestandar);
+        this.myForm.controls['modresili'].setValue(pavimento.modresili);
+        this.myForm.controls['servicini'].setValue(pavimento.servicini);
+        this.myForm.controls['servicfin'].setValue(pavimento.servicfin);
+        this.myForm.controls['numestruc'].setValue(pavimento.numestruc);
     }
 
-    calcular(myForm: FormGroup) {
+    onChangeAnyField(){
+        this.calcular(this.myForm)
+    }
 
+    //This function is necessary as workaround because (change) does not 
+    //updates myForm in select controls (bug)
+    onChangeConfiabilidadDiseno(newValue: any){
+        
+        console.log('onChangeConfiabilidadDiseno(..)');
+        if (newValue != '') {
+            let pavimento = this.formToPavimento(this.myForm);
+            pavimento.confiabdiseno = newValue;
+            console.log('Pavimento frm: ' + JSON.stringify(pavimento));
+
+            this.aashtoFlexibleService.calcular(pavimento).subscribe(data => pavimento = data);
+
+            this.myForm.controls['numestruc'].setValue(pavimento.numestruc);
+            console.log('Pavimento res: ' + JSON.stringify(pavimento));
+        }
+        else {
+           console.log('Form not valid');
+        }
+    }
+    
+    calcular(myForm: FormGroup) {
         console.log('calcular()');
+
         if (myForm.valid) {
 
             let pavimento = this.formToPavimento(myForm);
@@ -49,15 +93,16 @@ export class MainComponent implements OnInit {
 
             this.aashtoFlexibleService.calcular(pavimento).subscribe(data => pavimento = data);
 
-            this.fillFormWithPavimento(pavimento);
+            this.myForm.controls['numestruc'].setValue(pavimento.numestruc);
             console.log('Pavimento res: ' + JSON.stringify(pavimento));
         }
         else {
-            console.log('Form not valid');
+           console.log('Form not valid');
         }
     }
 
     private formToPavimento(myForm: FormGroup): Pavimento {
+        console.log('formToPavimento(...)');
         let pavimento: Pavimento = {
             ejesequiv: myForm.value.ejesequiv,
             confiabdiseno: myForm.value.confiabdiseno,
@@ -67,16 +112,17 @@ export class MainComponent implements OnInit {
             servicfin: myForm.value.servicfin,
             numestruc: myForm.value.numestruc,
         };
+        
         return pavimento;
     }
 
     private fillFormWithPavimento(pavimento: Pavimento) {
-        this.myForm.controls['ejesequiv'].setValue(pavimento.ejesequiv);
-        this.myForm.controls['confiabdiseno'].setValue(pavimento.confiabdiseno);
-        this.myForm.controls['errestandar'].setValue(pavimento.errestandar);
-        this.myForm.controls['modresili'].setValue(pavimento.modresili);
-        this.myForm.controls['servicini'].setValue(pavimento.servicini);
-        this.myForm.controls['servicfin'].setValue(pavimento.servicfin);
+        //this.myForm.controls['ejesequiv'].setValue(pavimento.ejesequiv);
+        //this.myForm.controls['confiabdiseno'].setValue(pavimento.confiabdiseno);
+        //this.myForm.controls['errestandar'].setValue(pavimento.errestandar);
+        //this.myForm.controls['modresili'].setValue(pavimento.modresili);
+        //this.myForm.controls['servicini'].setValue(pavimento.servicini);
+        //this.myForm.controls['servicfin'].setValue(pavimento.servicfin);
         this.myForm.controls['numestruc'].setValue(pavimento.numestruc);
     }
 
