89d9e6835807655c519139c0355f81996149db6e
Wolfgang Kling <wkling@rbsas.co>
2017-03-28 04:42:33 -0500

    Depuraciones para producción

--- src/client/app/module-one.module/usecase1/main.component.ts
+++ src/client/app/module-one.module/usecase1/main.component.ts
@@ -208,18 +208,8 @@ export class MainComponent implements OnInit {
         this.modal.alert()
             .size('lg')
             .showClose(true)
-            .title('A simple Alert style modal window')
-            .body(`
-            <h4>Alert is a classic (title/body/footer) 1 button modal window that 
-            does not block.</h4>
-            <b>Configuration:</b>
-            <ul>
-                <li>Non blocking (click anywhere outside to dismiss)</li>
-                <li>Size large</li>
-                <li>Dismissed with default keyboard key (ESC)</li>
-                <li>Close wth button click</li>
-                <li>HTML content</li>
-            </ul>`)
+            .title('Ejes equivalentes en el carril de diseño')
+            .body(``)
             .open();
     }
 }
