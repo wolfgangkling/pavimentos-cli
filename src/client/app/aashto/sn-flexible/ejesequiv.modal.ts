142b55cdb5552cb1a4e0453024af49bbb89dcf21
Wolfgang Kling <wkling@rbsas.co>
2017-03-30 18:16:21 -0500

    Se agrego ventana modal de ejes equialente y se organizaron componentes para que se vena ok

--- src/client/app/aashto/sn-flexible/ejesequiv.modal.ts
+++ src/client/app/aashto/sn-flexible/ejesequiv.modal.ts
@@ -15,45 +15,6 @@ export class EjesEquivalentesModalContext extends BSModalContext {
   moduleId: module.id,  
   selector: 'modal-content',
   templateUrl: './ejesequiv.modal.html',
-  styles: [`
-        .custom-modal-container {
-            padding: 15px;
-        }
-
-        .custom-modal-header {
-            background-color: #219161;
-            color: #fff;
-            -webkit-box-shadow: 0px 3px 5px 0px rgba(0,0,0,0.75);
-            -moz-box-shadow: 0px 3px 5px 0px rgba(0,0,0,0.75);
-            box-shadow: 0px 3px 5px 0px rgba(0,0,0,0.75);
-            margin-top: -15px;
-            margin-bottom: 40px;
-        }
-    `],
-  //TODO: [ngClass] here on purpose, no real use, just to show how to workaround ng2 issue #4330.
-  // Remove when solved.
-  /* tslint:disable */ 
-
-  /*
-  template: `
-        <div class="container-fluid custom-modal-container">
-            <div class="row custom-modal-header">
-                <div class="col-sm-12">
-                    <h1>A Custom modal design</h1>
-                </div>
-            </div>
-            <div class="row" [ngClass]="{'myclass' : shouldUseMyClass}">
-                <div class="col-xs-12">
-                    <div class="jumbotron">
-                        <h1>Do the math to quit:</h1>
-                        <p class="lead">I received an injection of the number <strong>{{context.num1}}</strong> and the number <strong>{{context.num2}}</strong></p>
-                        <span>What is the sum?</span>
-                         <input class="form-control" type="text" #answer (keyup)="onKeyUp(answer.value)" autofocus>
-                    </div>
-                </div>
-            </div>
-        </div>`
-        */
 })
 export class EjesEquivalentesModal implements CloseGuard, ModalComponent<EjesEquivalentesModalContext> {
   context: EjesEquivalentesModalContext;
@@ -63,7 +24,7 @@ export class EjesEquivalentesModal implements CloseGuard, ModalComponent<EjesEqu
   constructor(public dialog: DialogRef<EjesEquivalentesModalContext>) {
     this.context = dialog.context;
     this.wrongAnswer = true;
-    dialog.setCloseGuard(this);
+    //dialog.setCloseGuard(this);
   }
 
   onKeyUp(value: any) {
